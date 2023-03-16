6d0871e8c909af864df917ab0db0c44e32d4970c
Jagan Somanna <jagansomanna.bharath@kanini.com>
2023-03-16 18:32:19 +0530

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './layout/layout.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import{ MatIconModule} from '@angular/material/icon';
+import{MatSelectModule} from '@angular/material/select'
+import { ProjectsComponent } from './projects/projects.component';
+import { SddFooterComponent } from './sdd-footer/sdd-footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    ProjectsComponent,
+    SddFooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatSelectModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/sdd-footer/sdd-footer.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sdd-footer',
+  templateUrl: './sdd-footer.component.html',
+  styleUrls: ['./sdd-footer.component.scss']
+})
+export class SddFooterComponent {
+
+}
